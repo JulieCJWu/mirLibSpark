ae059a8a4b82ef8cff6d4d3f59007085a86efd30
Julie Wu <wu.chaojung@gmail.com>
2019-10-16 22:58:43 -0400

    change order of module load

--- workdir/local_mirlibspark_submission.sh
+++ workdir/local_mirlibspark_submission.sh
@@ -27,11 +27,11 @@
 #= included dependencies: miranda, VARNA
 #= viennarna will load python3.7, so need to reload python2.7.14 to override
 module load nixpkgs/16.09
-module load spark/2.4.4
 module load gcc/7.3.0
 module load viennarna/2.4.11
 module load bowtie/1.1.2
 module load blast+/2.7.1
+module load spark/2.4.4
 module load python/2.7.14
 
 
